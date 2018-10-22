2b60cae59e785d08e04844d5260f6e49c77ae826
Jared Sanders <jaredsanders@149-160-80-216.dhcp-bl.indiana.edu>
2018-10-22 11:32:04 -0400

    Cleaned up and added comments. Also uploaded files since server is now down

--- src/load_data.py
+++ src/load_data.py
@@ -2,20 +2,21 @@ import pandas as pd
 import tensorflow as tf
 
 TRAIN_URL = "http://18.221.149.34/files/csv/tensorflow/train.csv"
-TEST_URL = "http://18.221.149.34/files/csv/tensorflow/test.csv"
+TEST_URL = "http://18.221.149.34/files/csv/tensorflow/test.csv" # These no longer work, AWS was charging way too much. I have uploaded the files in question to the resources folder
 
+# The data I was given is inconsistent for the number of tests, so this may change drastically. 
 CSV_COLUMN_NAMES = ['t0', 't1',
                     't2', 't3', 't4', 't5', 't6','t7','t8','t9','t10','t11', 't12', 'Grades']
 GRADES = ['F', 'D', 'C', 'B', 'A']
 
-
+# Would actually download the files from my server if it was still on. Need to change to local
 def maybe_download():
     train_path = tf.keras.utils.get_file(TRAIN_URL.split('/')[-1], TRAIN_URL)
     test_path = tf.keras.utils.get_file(TEST_URL.split('/')[-1], TEST_URL)
 
     return train_path, test_path
 
-
+# Parsing the CSV
 def load_data(y_name='Grades'):
     """Returns the dataset as (train_x, train_y), (test_x, test_y)."""
     train_path, test_path = maybe_download()
@@ -28,7 +29,7 @@ def load_data(y_name='Grades'):
 
     return (train_x, train_y), (test_x, test_y)
 
-
+# Training steps
 def train_input_fn(features, labels, batch_size):
     """An input function for training"""
     # Convert the inputs to a Dataset.
@@ -40,9 +41,8 @@ def train_input_fn(features, labels, batch_size):
     # Return the dataset.
     return dataset
 
-
+# Takes input and evaluates
 def eval_input_fn(features, labels, batch_size):
-    """An input function for evaluation or prediction"""
     features = dict(features)
     if labels is None:
         # No labels, use only features.
@@ -61,14 +61,10 @@ def eval_input_fn(features, labels, batch_size):
     return dataset
 
 
-# The remainder of this file contains a simple example of a csv parser,
-#     implemented using a the `Dataset` class.
-
-# `tf.parse_csv` sets the types of the outputs to match the examples given in
-#     the `record_defaults` argument.
 CSV_TYPES = [[0.0], [0.0], [0.0], [0.0], [0]]
 
 
+# The below came mostly from Google's example. This does the initial reading of the CSV, the parser above simply puts it in (train_x, train_y) format
 def _parse_line(line):
     # Decode the line into its fields
     fields = tf.decode_csv(line, record_defaults=CSV_TYPES)
